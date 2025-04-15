bf482b1f3ee0f26541e1f147526f2a70b6b3cfb5
hitakiran <hamba2@illinois.edu>
2025-04-15 01:19:12 -0500

    Checkpoint 1 complete: Server can recieve message from client

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,23 @@
+use std::net::TcpStream;
+use std::io::{self, Write};
 
+fn main() {
+    let connection = TcpStream::connect("127.0.0.1:7878"); //connect to server
+
+    if let Ok(mut stream) = connection {
+        println!("Server is Connected! Type a message to send:");
+
+        let mut input = String::new();//hold user message
+
+        let user_input = io::stdin().read_line(&mut input); //read from terminal
+
+        if user_input.is_ok() {
+            let send_result = stream.write_all(input.as_bytes()); //convert the string to bytes
+            println!("Message is sent!");
+        } else {
+            println!("Message is NOT sent!");
+        }
+    } else {
+        println!("Can not connect to server!");
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,31 @@
+//importing: 
 
+use std::net::TcpListener;
+use std::io::Read;
+
+fn main() {
+    //bind server to IP address - local host port 7878
+    //listener waits for clients to connect to that address.
+    let listener = TcpListener::bind("127.0.0.1:7878").expect("Was not able to start the server. Sorry! :)");
+    println!("Server is listening on 127.0.0.1:7878");// started!
+
+    //loop through to take in incoming connections
+    //when connected, it gets a stream (connection) from listener
+    for stream_result in listener.incoming()  {
+        if let Ok(mut stream) = stream_result { //check 
+            println!("Client is connected!");
+
+            let mut buffer = [0; 512]; //storage
+
+            //read client messsage add it to buffer
+            let result = stream.read(&mut buffer);
+            result.expect("Can not read the client message!"); 
+
+            //fromutf8_lossy : converts bytes to string
+            let message = String::from_utf8_lossy(&buffer);
+
+            println!("Message from the client: {}", message); //prints to terminal
+        }
+    }
+
+}
